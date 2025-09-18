65e511a92b1e38358088c97448b315f4be811292
Dimas-Eko <dhimasprasetyo0@gmail.com>
2025-09-18 14:40:59 +0700

    Save before change

---
+++ src/routes/schedules.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const { createSchedule, getSchedules } = require('../controllers/schedules.controller');
+
+const mockAuth = (req, res, next)=>{
+    req.user = {id :'66f000000000000000000001'}
+
+    next()
+}
+
+router.post('/', mockAuth, createSchedule);
+router.get('/', mockAuth, getSchedules)
+
+
+module.exports = router;
